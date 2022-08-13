3633f444f1a6a20b70e5368d6f6661b26888b6bc
Steven Hastings <steve.hastings0321@gmail.com>
2022-08-13 05:18:11 -0400

    readme image

--- .stormenv/app.py
+++ .stormenv/app.py
@@ -1,7 +1,28 @@
-from flask import Flask, render_template
+import datetime
+from flask import Flask, render_template, request 
+from pymongo import MongoClient
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
+    client = MongoClient("mongodb+srv://stevenhastings:SuperStructure0321@manicblogger.9ojidle.mongodb.net/test")
+    app.db = client.ManicBlog
 
-@app.route("/")
-def hello_world():
-    return "Hello, world!"
+    @app.route("/", methods=["GET", "POST"])
+    def home():
+        print([e for e in app.db.entries.find({})])
+        if request.method == "POST":
+            entry_content = request.form.get("content")
+            formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
+            app.db.entries.insert_one({"content": entry_content, "date": formatted_date})
+
+        entries_with_date = [
+            (
+                entry["content"],
+                entry["date"],
+                datetime.datetime.strptime(entry["date"], "%Y-%m-%d").strftime("%b %d")
+            )
+            for entry in app.db.entries.find({})
+        ]
+        return render_template("home.html", entries=entries_with_date)    
+
+    return app
